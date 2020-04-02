cc7310fb62c66bb5fd90d4280b20795da7c73de7
Gordon Wallace <gordwilling@yahoo.ca>
2020-04-02 10:15:42 -0400

    double check against rubric

--- project-0/Task2.py
+++ project-0/Task2.py
@@ -24,37 +24,35 @@ September 2016.".
 """
 
 
-def call_duration_dict():
+def total_call_duration_dict():
     """
     Produces a dictionary where:
         key is a phone number
         value is the total duration the number was either on an incoming or outgoing call
     """
     call_durations = {}
-    for outgoing, call in groupby(numbers, key=itemgetter(0)):
+    for outgoing, call in groupby(calls, key=itemgetter(0)):
         if outgoing not in call_durations:
             call_durations[outgoing] = []
         else:
             call_durations[outgoing] += list(call)
 
-    for incoming, call in groupby(numbers, key=itemgetter(1)):
+    for incoming, call in groupby(calls, key=itemgetter(1)):
         if incoming not in call_durations:
             call_durations[incoming] = []
         else:
             call_durations[incoming] += list(call)
 
     total_call_durations = {}
-    for call, calls_for_number in call_durations.items():
-        durations = [int(call[1]) for call in calls_for_number]
-        duration = sum(durations)
-        total_call_durations[call] = duration
+    for number, calls_for_number in call_durations.items():
+        durations = [int(call[3]) for call in calls_for_number]
+        total_call_durations[number] = sum(durations)
 
     return total_call_durations
 
 
 if __name__ == '__main__':
-    numbers = [[call[0], call[3]] for call in calls]
-    total_durations = call_duration_dict()
+    total_durations = total_call_duration_dict()
     longest_caller = max(total_durations.items(), key=itemgetter(1))
     print(
         f"{longest_caller[0]} spent the longest time, {longest_caller[1]} seconds, on the phone during September 2016.")
