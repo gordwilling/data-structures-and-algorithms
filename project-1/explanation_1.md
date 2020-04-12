9cd4724654ec638babcc2a39ab23791839a27d35
Gordon Wallace <gordwilling@yahoo.ca>
2020-04-12 15:13:31 -0400

    Rewrite to accommodate feedback. Uses a doubly-linked list to enable O(1) removal at any index

--- project-1/explanation_1.md
+++ project-1/explanation_1.md
@@ -4,10 +4,21 @@
 
 The LRU cache provides get and set operations with O(1) time complexity and O(n) space complexity.
 
-Key-value pairs can be stored in a python Dictionary which meets these complexity requirements. Key values are
-also stored in a Queue, which provides O(1) time access to the first and last elements in the collection. When the
-cache is accessed (via get or set):
+A doubly-linked-list is used as a queue which enables O(1) access to the head and tail, as well as O(1) 
+removal (given a reference to the node being removed)
 
-1. If the queue is full, the item at the front of the queue (the least recently used item) is removed to make space
-1. The key is considered *used* and added to the end of the queue (the position representing the most recently used item)
-1. The value is either inserted into or retrieved from the cache.
+A python dictionary maintains key-value pairs where the values point to nodes in the linked list.
+
+When an existing value is accessed via get:
+
+1. The reference to its node is looked up in the dictionary: O(1)
+1. The node is promoted to the head (most-recently-used position) of the queue in an O(1) operation made possible by the 
+   references maintained in the data structures.
+  
+When an item is set:
+
+1. If the queue is at capacity, the item at the tail of the list (the least recently used item) is removed to make space. 
+   This is O(1) because the doubly-linked-list maintains a reference to its tail
+1. The item is considered *used* and is set as the head of the list (the position representing the most recently used item).
+   This is also O(1) because the doubly-linked-list maintaines a reference to its head
+
--- project-1/problem_1.py
+++ project-1/problem_1.py
@@ -1,45 +1,130 @@
-from queue import Queue
+import sys
+
+
+class Node(object):
+    def __init__(self, value, next=None, previous=None):
+        self.value = value
+        self.next = next
+        self.previous = previous
+
+
+class DoublyLinkedList(object):
+    def __init__(self):
+        self.root = None
+        self.tail = None
+
+    def add(self, value) -> Node:
+        if not self.root:
+            self.root = Node(value)
+            self.tail = self.root
+        else:
+            old_root = self.root
+            self.root = Node(value)
+            self.root.next = old_root
+            old_root.previous = self.root
+        return self.root
+
+    def drop(self) -> Node:
+        if self.tail:
+            node = self.tail
+            self.tail = self.tail.previous
+            if self.tail:
+                self.tail.next = None
+            return node
+
+    def remove(self, node):
+        if node == self.root:
+            self.root = None
+            self.tail = None
+        elif node == self.tail:
+            self.tail = self.tail.previous
+            self.tail.next = None
+        else:
+            if node.previous:
+                node.previous.next = node.next
+                node.next.previous = node.previous
+
+    def __str__(self):
+        contents = []
+        node = self.root
+        while node:
+            contents.append(node.value)
+            node = node.next
+        return str(contents)
+
+
+class CacheValue(object):
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
 
 
 class LRU_Cache(object):
 
     def __init__(self, capacity):
-        self.lru = Queue(capacity)
+        if capacity < 0:
+            raise ValueError("Capacity cannot be negative")
+
+        if capacity == 0:
+            raise ValueError("Capacity cannot be zero")
+
+        self.lru = DoublyLinkedList()
         self.cache = {}
         self.capacity = capacity
         self.count = 0
 
     def get(self, key):
         if key in self.cache:
-            self._add_most_recently_used(key)
-            return self.cache[key]
+            node = self.cache[key]
+            self.lru.remove(node)
+            self.cache[key] = self.lru.add(node.value)
+            return node.value.value
         else:
             return -1
 
     def set(self, key, value):
-        self._add_most_recently_used(key)
-        self.cache[key] = value
-
-    def _add_most_recently_used(self, key):
-        self._ensure_capacity()
-        self.lru.put(key)
-        self.count += 1
-
-    def _ensure_capacity(self):
-        if self.capacity == self.count:
-            key = self.lru.get_nowait()
-            del self.cache[key]
-            self.count -= 1
+        if key in self.cache:
+            node = self.cache[key]
+            self.lru.remove(node)
+            self.cache[key] = self.lru.add(CacheValue(key, value))
+        else:
+            if self.count == self.capacity:
+                node = self.lru.drop()
+                if node:
+                    del self.cache[node.value.key]
+                    self.count -= 1
+            self.cache[key] = self.lru.add(CacheValue(key, value))
+            self.count += 1
 
 
 if __name__ == '__main__':
+    try:
+        LRU_Cache(-1)
+    except ValueError:
+        print(f"{sys.exc_info()[0].__name__}: {sys.exc_info()[1]}")
+    # ValueError: Capacity cannot be negative
+
+    try:
+        LRU_Cache(0)
+    except ValueError:
+        print(f"{sys.exc_info()[0].__name__}: {sys.exc_info()[1]}")
+    # ValueError: Capacity cannot be zero
+
+    cache_of_one = LRU_Cache(1)
+    cache_of_one.set(1, "One")
+    print(cache_of_one.get(1))
+    # One
+
+    cache_of_one.set(2, "Two")
+    print(cache_of_one.get(2))
+    # Two
+
     our_cache = LRU_Cache(5)
 
     print(our_cache.get(0))
     # -1
 
     print(our_cache.get(None))
-    # -1
 
     our_cache = LRU_Cache(5)
     our_cache.set(1, "One")
